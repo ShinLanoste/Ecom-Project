b86d98cabb43f4fe64a5924fa34dff0c8c2d3b7f
ShinLanoste <tamangamrit0001@gmail.com>
2024-12-07 14:52:31 +0545

    Practice number 2

--- practice.js
+++ practice.js
@@ -1,11 +1,18 @@
-// Check whether the Input is an array or not
-const checkFunction = (input) => {
-  if (Object.prototype.toString.call(input) === "[object Array]") {
-    return true;
-  } else {
-    return false;
-  }
-};
+//1. Check whether the Input is an array or not
+// const checkFunction = (input) => {
+//   if (Object.prototype.toString.call(input) === "[object Array]") {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
 
-console.log(checkFunction("Ohayogojaimasu!!"));
-console.log(checkFunction([1, 2, 3, 5, 6]));
+// console.log(checkFunction("Ohayogojaimasu!!"));
+// console.log(checkFunction([1, 2, 3, 5, 6]));
+
+// 2. Cloning the array
+// let array = (arr) => {
+//   return arr.slice();
+// };
+// console.log(array([1, 2, 3, 4, 5]));
+// console.log(array([1, 2, [4, 0]]));
