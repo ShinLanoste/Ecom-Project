be6b025896629d4d3dedf2461faf7d85a03a6724
ShinLanoste <tamangamrit0001@gmail.com>
2024-12-07 15:05:53 +0545

    Practice number 3

--- practice.js
+++ practice.js
@@ -16,3 +16,16 @@
 // };
 // console.log(array([1, 2, 3, 4, 5]));
 // console.log(array([1, 2, [4, 0]]));
+
+// 3. JavaScript function to get the first element of an array.
+// Passing the parameter 'n' will return the first 'n' elements of the array.
+// let container = (arr, n) => {
+//   if (n === undefined) {
+//     return arr[0];
+//   } else {
+//     return arr.slice(0, n);
+//   }
+// };
+
+// console.log(container([1, 2, 3, 4, 5]));
+// console.log(container([6, 7, 8, 9], 3));
